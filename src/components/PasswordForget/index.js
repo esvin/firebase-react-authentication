588aad3189f883f2b82e32e6df8593c7560e6dec
Esvin Jose Estrada <esvin.estradasoc@conduent.com>
2020-10-04 22:31:20 -0600

    feat: building boiler plate

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PasswordForgetPage = () => (
+    <div>
+        <h1>Hola que tal america</h1>
+    </div>
+);
+
+export default PasswordForgetPage;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUpPage = () => (
+    <div>
+        <h1>Sign Up here folks</h1>
+    </div>
+);
+
+export default SignUpPage;
